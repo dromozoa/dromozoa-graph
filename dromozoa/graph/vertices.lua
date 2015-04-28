bdf35a726fddcfca491c59d5c0c7548c25f1879b
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-04-28 18:41:26 +0900

    リファクタリング。

--- dromozoa/graph/vertices.lua
+++ dromozoa/graph/vertices.lua
@@ -18,45 +18,45 @@
 local clone = require "dromozoa.graph.clone"
 local vertex = require "dromozoa.graph.vertex"
 
-local function each_vertex(ctx, v)
-  return ctx:get_vertex(next(ctx._v, v and v.id))
-end
+local function construct(self, g, data, n)
+  local p = g._vp
 
-local function construct(self)
   function self:clone(g)
-    return construct {
-      _g = g;
-      _n = self._n;
-      _v = clone(self._v);
-    }
+    return construct({}, g, clone(data), n)
   end
 
   function self:empty()
-    return not next(self._v)
+    return not next(data)
   end
 
   function self:create_vertex()
-    local id = self._n + 1
-    self._n = id
-    self._v[id] = true
-    return vertex(self._g, id)
+    local id = n
+    n = id + 1
+    data[id] = true
+    return vertex(g, id)
   end
 
   function self:remove_vertex(id)
-    self._v[id] = nil
+    data[id] = nil
   end
 
   function self:get_vertex(id)
     if id then
-      return vertex(self._g, id)
+      return vertex(g, id)
     end
   end
 
-  function self:each_vertex(k)
-    if k then
-      return self._g._vp:each_item(k, self.get_vertex, self)
+  function self:each_vertex(key)
+    if key then
+      return p:each_item(key, self.get_vertex, self)
     else
-      return each_vertex, self
+      return function (_, i)
+        if i then
+          return self:get_vertex(next(data, i.id))
+        else
+          return self:get_vertex(next(data))
+        end
+      end
     end
   end
 
@@ -64,9 +64,5 @@ local function construct(self)
 end
 
 return function (g)
-  return construct {
-    _g = g;
-    _n = 0;
-    _v = {};
-  }
+  return construct({}, g, {}, 1)
 end
