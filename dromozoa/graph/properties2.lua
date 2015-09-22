7bebd3e19e9f31c17f867eb650b48b69e3a17e0f
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-09-22 10:51:11 +0900

    propertiesの実装を追加。

---
+++ dromozoa/graph/properties2.lua
@@ -0,0 +1,91 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-graph.
+--
+-- dromozoa-graph is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-graph is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-graph.  If not, see <http://www.gnu.org/licenses/>.
+
+local pairs = require "dromozoa.commons.pairs"
+
+local class = {}
+
+function class.new()
+  return {
+    dataset = {};
+  }
+end
+
+function class:clear_properties(key)
+  self.dataset[key] = nil
+end
+
+function class:remove_item(id)
+  for key, data in pairs(self.dataset) do
+    data[id] = nil
+    if next(data) == nil then
+      self[key] = nil
+    end
+  end
+end
+
+function class:each_item(key)
+  local data = self.dataset[key]
+  if data then
+    return next, data, nil
+  else
+    return function () end
+  end
+end
+
+function class:set_property(id, key, value)
+  local dataset = self.dataset
+  local data = dataset[key]
+  if data then
+    data[id] = value
+    if next(data) == nil then
+      dataset[key] = nil
+    end
+  else
+    if value ~= nil then
+      dataset[key] = { [id] = value }
+    end
+  end
+end
+
+function class:get_property(id, key)
+  local data = self.dataset[key]
+  if data then
+    return data[id]
+  end
+end
+
+function class:each_property(id)
+  return coroutine.wrap(function ()
+    for key, data in pairs(self.dataset) do
+      local value = data[id]
+      if value ~= nil then
+        coroutine.yield(key, value)
+      end
+    end
+  end)
+end
+
+local metatable = {
+  __index = class;
+}
+
+return setmetatable(class, {
+  __call = function ()
+    return setmetatable(class.new(), metatable)
+  end;
+})
