78284d9b409c6b2127eebfafa149c3f0854b7312
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-11-03 12:53:06 +0900

    マイクロベンチマークを追加。

---
+++ bench/ubench_longest_path.lua
@@ -0,0 +1,53 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-graph.
+--
+-- dromozoa-graph is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-graph is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-graph.  If not, see <http://www.gnu.org/licenses/>.
+
+local graph = require "dromozoa.graph"
+local longest_path = require "experimental.longest_path"
+
+local N = 1000
+
+local function run(f, g)
+  local reverse = f(g)
+  return f, g, reverse
+end
+
+local g = graph()
+local uid = g:add_vertex()
+for i = 1, N do
+  local vid1 = g:add_vertex()
+  local vid2 = g:add_vertex()
+  local vid3 = g:add_vertex()
+
+  g:add_edge(uid, vid1)
+  g:add_edge(uid, vid2)
+  g:add_edge(vid1, vid3)
+  g:add_edge(vid2, vid3)
+
+  uid = vid3
+end
+
+local algorithms = {
+  longest_path;
+}
+
+local benchmarks = {}
+
+for i = 1, #algorithms do
+  benchmarks[("%02d"):format(i)] = { run, algorithms[i], g }
+end
+
+return benchmarks
