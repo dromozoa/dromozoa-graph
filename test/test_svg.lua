762fa7b1dbea1b5c2a8e5b1cbbf541343ec1f425
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-11-20 18:32:43 +0900

    テストを追加。

---
+++ test/test_svg.lua
@@ -0,0 +1,38 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-graph.
+--
+-- dromozoa-graph is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-graph is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-graph.  If not, see <http://www.gnu.org/licenses/>.
+
+io.write([[
+<svg version="1.1" width="600" height="600" xmlns="http://www.w3.org/2000/svg">
+<style>
+@font-face {
+  font-family: 'Noto Sans Mono CJK JP';
+  font-style: normal;
+  font-weight: 400;
+  src: url('https://dromozoa.s3.amazonaws.com/mirror/NotoSansCJKjp-2017-04-03/NotoSansMonoCJKjp-Regular.otf') format('opentype');
+}
+
+text {
+  font-family: 'Noto Sans Mono CJK JP';
+  font-weight: 400;
+  dominant-baseline: central;
+  text-anchor: middle;
+}
+</style>
+<circle cx="300" cy="300" r="100" stroke="black" fill="white"/>
+<text x="300" y="300" stroke="none" fill="black">ABC日本語</text>
+</svg>
+]])
