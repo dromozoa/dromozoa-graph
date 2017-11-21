c2b4f16d632d4b0b931997d5560f489d23864c7d
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-11-21 09:27:14 +0900

    Javaプロジェクトを修正。

--- test/icu4j/src/main/java/com/dromozoa/graph/App.java
+++ test/icu4j/src/main/java/com/dromozoa/graph/App.java
@@ -1,13 +1,6 @@
 package com.dromozoa.graph;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+  }
 }
