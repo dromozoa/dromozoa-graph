2248328c694b6aa2f30d386be89022002a729419
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-25 15:14:53 +0900

    判りづらい最適化を廃止。

--- dromozoa/graph/brandes_kopf.lua
+++ dromozoa/graph/brandes_kopf.lua
@@ -231,6 +231,152 @@ local function vertical_alignment(u, vu, layers, index_map, mark, layer_first, l
   return root, align
 end
 
+
+local function vertical_alignment(u, vu, layers, index_map, mark, layer_first, layer_last, layer_step, left)
+  local u_after = u.after
+  local vu_first = vu.first
+  local vu_after = vu.after
+  local vu_target = vu.target
+
+  local compare
+  if left then
+    compare = function (eid1, eid2)
+      return index_map[vu_target[eid1]] < index_map[vu_target[eid2]]
+    end
+  else
+    compare = function (eid1, eid2)
+      return index_map[vu_target[eid1]] > index_map[vu_target[eid2]]
+    end
+  end
+
+  local root = {}
+  local align = {}
+
+  local uid = u.first
+  while uid do
+    root[uid] = uid
+    align[uid] = uid
+    uid = u_after[uid]
+  end
+
+  local first
+  local last
+  local step
+  if left then
+    first = 1
+    step = 1
+  else
+    last = 1
+    step = -1
+  end
+
+  local eids = {}
+  local condition
+
+  for i = layer_first, layer_last, layer_step do
+    local uids = layers[i]
+
+    if left then
+      last = #uids
+    else
+      first = #uids
+    end
+
+    local a
+
+    for j = first, last, step do
+      local uid = uids[j]
+      local en = 0
+
+      local eid = vu_first[uid]
+      while eid do
+        en = en + 1
+        eids[en] = eid
+        eid = vu_after[eid]
+      end
+
+      if en > 0 then
+        if align[uid] == uid then
+          for k = en + 1, #eids do
+            eids[k] = nil
+          end
+          sort(eids, compare)
+
+          if en % 2 == 1 then
+            local m = (en + 1) / 2
+            local eid = eids[m]
+            if not mark[eid] then
+              local vid = vu_target[eid]
+              local b = index_map[vid]
+              local condition
+              if not a then
+                condition = true
+              elseif left then
+                condition = a < b
+              else
+                condition = a > b
+              end
+              if condition then
+                local wid = root[vid]
+                root[uid] = wid
+                align[vid] = uid
+                align[uid] = wid
+                a = b
+              end
+            end
+          else
+            local m = en / 2
+            local eid = eids[m]
+            if not mark[eid] then
+              local vid = vu_target[eid]
+              local b = index_map[vid]
+              local condition
+              if not a then
+                condition = true
+              elseif left then
+                condition = a < b
+              else
+                condition = a > b
+              end
+              if condition then
+                local wid = root[vid]
+                root[uid] = wid
+                align[vid] = uid
+                align[uid] = wid
+                a = b
+              end
+            end
+
+            if align[uid] == uid then
+              local eid = eids[m + 1]
+              if not mark[eid] then
+                local vid = vu_target[eid]
+                local b = index_map[vid]
+                if not a then
+                  condition = true
+                elseif left then
+                  condition = a < b
+                else
+                  condition = a > b
+                end
+                if condition then
+                  local wid = root[vid]
+                  root[uid] = wid
+                  align[vid] = uid
+                  align[uid] = wid
+                  a = b
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  return root, align
+end
+
 local function place_block(layer_map, layers, index_map, root, align, left, sink, shift, x, vid)
   if not x[vid] then
     x[vid] = 0
