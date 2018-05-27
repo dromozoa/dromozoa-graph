0d4c0cea938ce90c1a2e61e830fd0791286d72d8
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-28 08:40:25 +0900

    交差最小化を修正。

--- dromozoa/graph/minimize_crossings.lua
+++ dromozoa/graph/minimize_crossings.lua
@@ -17,6 +17,8 @@
 
 local count_crossings = require "dromozoa.graph.count_crossings"
 
+local sort = table.sort
+
 local function copy(source_orders, target_orders)
   for i = 1, #source_orders do
     local source_order = source_orders[i]
@@ -27,18 +29,73 @@ local function copy(source_orders, target_orders)
   end
 end
 
-local function wmedian(g, orders, iter)
-  if iter % 2 == 0 then
-    for i = 1, #orders do
-      local order = orders[i]
-      local median = {}
-      for j = 1, #order do
-        local uid = order[j]
-        median[uid] = median_value(g, uid, i - 1)
-      end
+local function wmedian(vu, orders, order_first, order_last, order_step)
+  local vu_first = vu.first
+  local vu_after = vu.after
+  local vu_target = vu.target
 
+  -- 最初の階層はなにもしない
+  for i = order_first + order_step, order_last, order_step do
+    local order1 = orders[i - order_step]
+    local order2 = orders[i]
+
+    local order_map = {}
+    for j = 1, #order1 do
+      order_map[order1[j]] = j
     end
-  else
+
+    local new_order = {}
+    local sequence = {}
+
+    for j = 1, #order2 do
+      local uid = order2[j]
+      local s = {}
+      local eid = vu_first[uid]
+      while eid do
+        s[#s + 1] = order_map[vu_target[eid]]
+        eid = vu_after[eid]
+      end
+      sort(s)
+      local m = #s
+      if m == 0 then
+        new_order[j] = uid
+      elseif m % 2 == 1 then
+        sequence[#sequence + 1] = {
+          uid = uid;
+          median = s[(m + 1) / 2];
+        }
+      elseif m == 2 then
+        sequence[#sequence + 1] = {
+          uid = uid;
+          median = (s[1] + s[2]) / 2;
+        }
+      else
+        local n = m / 2
+        local a = s[n]
+        local b = s[n + 1]
+        local left = a - s[1]
+        local right = s[m] - b
+        sequence[#sequence + 1] = {
+          uid = uid;
+          median = (a * right + b * left) / (left + right);
+        }
+      end
+    end
+
+    sort(sequence, function (a, b)
+      return a.median < b.median
+    end)
+
+    local n = 0
+    for j = 1, #sequence do
+      while true do
+        n = n + 1
+        if not new_order[n] then
+          new_order[n] = sequence[j].uid
+          break
+        end
+      end
+    end
   end
 end
 
@@ -54,14 +111,20 @@ local function crossing(g, orders)
 end
 
 return function (g, orders)
+  local n = #orders
+
   local best = {}
-  for i = 1, #orders do
+  for i = 1, n do
     best[i] = {}
   end
   copy(orders, best)
 
-  for i = 1, 24 do
-    wmedian(g, orders, i)
+  for i = 1, 1 do
+    wmedian(g.vu, orders, 1, n, 1)
+    if crossing(g, orders) < crossing(g, best) then
+      copy(orders, best)
+    end
+    wmedian(g.uv, orders, n, 1, -1)
     if crossing(g, orders) < crossing(g, best) then
       copy(orders, best)
     end
