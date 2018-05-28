0bfbaf4e7fc74605dc81e63248949f26d99b67d1
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-05-28 18:17:00 +0900

    リファクタリング。

--- dromozoa/graph/minimize_crossings.lua
+++ dromozoa/graph/minimize_crossings.lua
@@ -29,14 +29,14 @@ local function copy(source_layers, target_layers)
   end
 end
 
-local function wmedian(uv, layers, layer_first, layer_last, layer_step)
+local function median(uv, layers, layer_first, layer_last, layer_step)
   local uv_first = uv.first
   local uv_after = uv.after
   local uv_target = uv.target
 
+  local south = layers[layer_first]
   for i = layer_first + layer_step, layer_last, layer_step do
     local north = layers[i]
-    local south = layers[i - layer_step]
 
     local order_map = {}
     for j = 1, #south do
@@ -44,59 +44,66 @@ local function wmedian(uv, layers, layer_first, layer_last, layer_step)
     end
 
     local order = {}
-    local sequence = {}
+    local median_map = {}
+    local p = {}
 
     for j = 1, #north do
       local uid = north[j]
-      local s = {}
+      local p = {}
+      local m = 0
       local eid = uv_first[uid]
       while eid do
-        s[#s + 1] = order_map[uv_target[eid]]
+        m = m + 1
+        p[m] = order_map[uv_target[eid]]
         eid = uv_after[eid]
       end
-      sort(s)
-      local m = #s
-      if m == 0 then
-        order[j] = uid
-      elseif m % 2 == 1 then
-        sequence[#sequence + 1] = {
-          uid = uid;
-          median = s[(m + 1) / 2];
-        }
-      elseif m == 2 then
-        sequence[#sequence + 1] = {
-          uid = uid;
-          median = (s[1] + s[2]) / 2;
-        }
+      for k = m + 1, #p do
+        p[k] = nil
+      end
+      north[j] = nil
+      if m < 3 then
+        if m == 0 then
+          north[j] = uid
+        elseif m == 1 then
+          order[#order + 1] = uid
+          median_map[uid] = p[1]
+        else
+          order[#order + 1] = uid
+          median_map[uid] = (p[1] + p[2]) / 2
+        end
       else
-        local n = m / 2
-        local a = s[n]
-        local b = s[n + 1]
-        local left = a - s[1]
-        local right = s[m] - b
-        sequence[#sequence + 1] = {
-          uid = uid;
-          median = (a * right + b * left) / (left + right);
-        }
+        sort(p)
+        if m % 2 == 1 then
+          order[#order + 1] = uid
+          median_map[uid] = p[(m + 1) / 2]
+        else
+          local n = m / 2
+          local a = p[n]
+          local b = p[n + 1]
+          local left = a - p[1]
+          local right = p[m] - b
+          order[#order + 1] = uid
+          median_map[uid] = (a * right + b * left) / (left + right)
+        end
       end
     end
 
-    sort(sequence, function (a, b)
-      return a.median < b.median
+    sort(order, function (a, b)
+      return median_map[a] < median_map[b]
     end)
 
     local n = 0
-    for j = 1, #sequence do
+    for j = 1, #order do
       while true do
         n = n + 1
-        if not order[n] then
-          order[n] = sequence[j].uid
+        if not north[n] then
+          north[n] = order[j]
           break
         end
       end
     end
 
-    layers[i] = order
+    south = north
   end
 end
 
@@ -156,12 +163,12 @@ return function (g, layers)
   copy(layers, best)
 
   for i = 1, 12 do
-    wmedian(g.uv, layers, 1, n, 1)
+    median(g.uv, layers, 1, n, 1)
     transpose(g, layers)
     if crossing_g(g, layers) < crossing_g(g, best) then
       copy(layers, best)
     end
-    wmedian(g.vu, layers, n, 1, -1)
+    median(g.vu, layers, n, 1, -1)
     transpose(g, layers)
     if crossing_g(g, layers) < crossing_g(g, best) then
       copy(layers, best)
